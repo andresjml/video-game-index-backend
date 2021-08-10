71f82f41126d0a9e5b46d8de455c9984e501b55c
Andres <andresjml@gmail.com>
2021-08-10 12:30:33 -0400

    rewritting routes

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,62 @@ class Application
     res = Rack::Response.new
     req = Rack::Request.new(env)
 
+    # GAME RESTful ROUTES
+
+    # Game Index
+
+    if req.path == '/games' && req.get?
+      games=Game.all
+      return [
+        200, 
+        { 'Content-Type' => 'application/json' }, 
+        [ games.to_json ]
+      ]    
+    end
+
+    # Game Create
+
+    if req.path.match(/games/) && req.post?
+      body=JSON.parse(req.body.read)
+      game=Game.create(body)
+      return [
+        200, 
+        { 'Content-Type' => 'application/json' }, 
+        [ game.to_json ]
+      ] 
+    end
+
+    # Game Show
+
+    if req.path.match(/games/) && req.get?
+      id=req.path.split('/')[2]
+      game=Game.find_by_id(id)
+      if game
+        platforms=game.platforms
+        game_resp = {
+          title: game.title,
+          description: game.description,
+          genre:game.genre.name,
+          platforms:platforms
+        }
+        return [
+          200, 
+          { 'Content-Type' => 'application/json' }, 
+          [ game_resp.to_json ]
+        ]
+      else
+        return [
+          404, 
+          { 'Content-Type' => 'application/json' }, 
+          [ {error: "Game not Found"}.to_json ]
+        ]
+      end 
+    end
+
+    # Game Update
+
+    # Game Delete
+
     if req.path == '/genres' && req.get?
       genres=Genre.all
       return [
@@ -33,36 +89,8 @@ class Application
         ]
       end 
     
-    elsif req.path == '/games' && req.get?
-      games=Game.all
-      return [
-        200, 
-        { 'Content-Type' => 'application/json' }, 
-        [ games.to_json ]
-      ]    
-    elsif req.path.match(/games/) && req.get?
-      id=req.path.split('/')[2]
-      game=Game.find_by_id(id)
-      if game
-        platforms=game.platforms
-        game_resp = {
-          title: game.title,
-          description: game.description,
-          genre:game.genre.name,
-          platforms:platforms
-        }
-        return [
-          200, 
-          { 'Content-Type' => 'application/json' }, 
-          [ game_resp.to_json ]
-        ]
-      else
-        return [
-          404, 
-          { 'Content-Type' => 'application/json' }, 
-          [ {error: "Game not Found"}.to_json ]
-        ]
-      end 
+    
+    
     elsif req.path == '/platforms' && req.get?
       platforms=Platform.all
       return [
