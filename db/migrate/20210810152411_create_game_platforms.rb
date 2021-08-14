a7bd2056c10db0270fb4c03aa38792c9fb845591
Andres <andresjml@gmail.com>
2021-08-13 21:53:44 -0400

    postgresql configured

--- db/migrate/20210810152411_create_game_platforms.rb
+++ db/migrate/20210810152411_create_game_platforms.rb
@@ -1,8 +1,8 @@
 class CreateGamePlatforms < ActiveRecord::Migration[5.2]
     def change
       create_table :game_platforms do |t|
-        t.integer :game_id
-        t.integer :platform_id
+        t.belongs_to :game_id, foreign_key: true
+        t.belongs_to :platform_id, foreign_key: true        
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_10_152411) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "game_platforms", force: :cascade do |t|
     t.integer "game_id"
     t.integer "platform_id"
